e5d00e78b1ad1b230563900a82f6ed39f22a24f1
test <754266963@qq.com>
2018-04-11 18:39:39 +0800

    111

--- src/base/ErrorType.go
+++
@@ -1,7 +0,0 @@
-package base
-
-const (
-	NONE_ERROR = iota
-	VERSION_ERROR = iota				//版本不正确
-	ACCOUNT_NOEXIST = iota			//账号不存在
-)
--- src/server/world/data/InitData.go
+++
@@ -1,5 +0,0 @@
-package data
-
-func InitRepository(){
-	BANDREPOSITORY.Read()
-}
