e36f23966db06054a1e02a51434419d198249b51
yanlingbo <754266963@qq.com>
2019-04-30 16:09:02 +0800

    修复log文件，输出源码异常，增加err输出

--- src/gonet/base/log.go
+++ src/gonet/base/log.go
@@ -1,23 +1,27 @@
 package base
 
 import (
-	"os"
-	"log"
-	"time"
 	"fmt"
+	"log"
+	"os"
 	"sync"
+	"time"
 )
 
+type LG_TYPE int
 const (
-	LG_WARN = iota
-	LG_ERROR = iota
+	LG_WARN  LG_TYPE = iota
+	LG_ERROR LG_TYPE = iota
+	LG_MAX LG_TYPE = iota
+)
+const(
 	PATH = "log"
 )
 
 type (
 	CLog struct {
-		log.Logger
-		m_pFile *os.File
+		m_Logger [LG_MAX]log.Logger
+		m_pFile [LG_MAX]*os.File
 		m_Time time.Time
 		m_FileName string
 		m_LogSuffix string
@@ -42,7 +46,7 @@ var(
 )
 
 func (this *CLog) Init(fileName string) bool {
-	this.m_pFile = nil
+	//this.m_pFile = nil
 	this.m_FileName = fileName
 	this.m_LogSuffix = "log"
 	this.m_ErrSuffix = "err"
@@ -50,10 +54,18 @@ func (this *CLog) Init(fileName string) bool {
 	return true
 }
 
-func (this *CLog)Write(nType int){
+func (this *CLog) GetSuffix(nType LG_TYPE) string{
+	if nType == LG_WARN{
+		return this.m_LogSuffix
+	}else{
+		return this.m_ErrSuffix
+	}
+}
+
+func (this *CLog)Write(nType LG_TYPE){
 	this.WriteFile(nType)
 	tTime := time.Now()
-	this.SetPrefix(fmt.Sprintf("[%04d-%02d-%02d %02d:%02d:%02d]",tTime.Year(), tTime.Month(), tTime.Day(),
+	this.m_Logger[nType].SetPrefix(fmt.Sprintf("[%04d-%02d-%02d %02d:%02d:%02d]",tTime.Year(), tTime.Month(), tTime.Day(),
 		tTime.Hour(), tTime.Minute(), tTime.Second()))
 }
 
@@ -64,7 +76,7 @@ func (this *CLog) Println(v1 ...interface{}) {
 		params[i] = v
 	}
 	params[len(v1)] = "\r\n"
-	this.Logger.Println(params...)
+	this.m_Logger[LG_WARN].Output(2, fmt.Sprintln(params...))
 	log.Println(params...)
 }
 
@@ -75,14 +87,14 @@ func (this *CLog) Print(v1 ...interface{}) {
 		params[i] = v
 	}
 	params[len(v1)] = "\r\n"
-	this.Logger.Print(params...)
+	this.m_Logger[LG_WARN].Output(2, fmt.Sprint(params...))
 	log.Print(params...)
 }
 
 func (this *CLog) Printf(format string, params ...interface{}) {
 	this.Write(LG_WARN)
 	format += "\r\n";
-	this.Logger.Printf(format, params...)
+	this.m_Logger[LG_WARN].Output(2, fmt.Sprintf(format, params...))
 	log.Printf(format,params...)
 }
 
@@ -93,7 +105,7 @@ func (this *CLog) Fatalln(v1 ...interface{}) {
 		params[i] = v
 	}
 	params[len(v1)] = "\r\n"
-	this.Logger.Println(params...)
+	this.m_Logger[LG_ERROR].Output(2, fmt.Sprintln(params...))
 }
 
 func (this *CLog) Fatal(v1 ...interface{}) {
@@ -103,54 +115,46 @@ func (this *CLog) Fatal(v1 ...interface{}) {
 		params[i] = v
 	}
 	params[len(v1)] = "\r\n"
-	this.Logger.Print(params...)
+	this.m_Logger[LG_ERROR].Output(2, fmt.Sprint(params...))
 }
 
 func (this *CLog) Fatalf(format string, params ...interface{}) {
 	this.Write(LG_ERROR)
 	format += "\r\n"
-	this.Logger.Printf(format, params...)
+	this.m_Logger[LG_ERROR].Output(2, fmt.Sprintf(format, params...))
 }
 
-func (this *CLog) WriteFile(nType int){
+func (this *CLog) WriteFile(nType LG_TYPE){
 	var err error
 	tTime := time.Now()
-	if (this.m_pFile == nil ||  this.m_Time.Year() != tTime.Year() ||
+	if (this.m_pFile[nType] == nil ||  this.m_Time.Year() != tTime.Year() ||
 	this.m_Time.Month() != tTime.Month() || this.m_Time.Day() != tTime.Day()){
 		this.m_Loceker.Lock()
-		if this.m_pFile != nil {
-			defer this.m_pFile.Close()
+		if this.m_pFile[nType] != nil {
+			defer this.m_pFile[nType].Close()
 		}
 
 		if PathExists(PATH) == false {
 			os.Mkdir(PATH, os.ModeDir)
 		}
 
-		GetSuffix := func(nType int) string{
-			if nType == LG_WARN{
-				return this.m_LogSuffix
-			}else{
-				return this.m_ErrSuffix
-			}
-		}
-
 		sFileName := fmt.Sprintf("%s/%s_%d%02d%02d.%s", PATH, this.m_FileName,tTime.Year(), tTime.Month(), tTime.Day(),
-			GetSuffix(nType))
+			this.GetSuffix(nType))
 
 		if PathExists(sFileName) == false{
-			os.Create(sFileName);
+			os.Create(sFileName)
 		}
 
-		this.m_pFile,err  = os.OpenFile(sFileName, os.O_RDWR, 0)
+		this.m_pFile[nType],err  = os.OpenFile(sFileName, os.O_RDWR|os.O_APPEND, 0)
 		if err != nil {
 			log.Fatalln("open logfile[%s] error", sFileName)
 		}
 
-		this.SetOutput(this.m_pFile)
-		this.SetPrefix(fmt.Sprintf("[%04d-%02d-%02d %02d:%02d:%02d]",tTime.Year(), tTime.Month(), tTime.Day(),
+		this.m_Logger[nType].SetOutput(this.m_pFile[nType])
+		this.m_Logger[nType].SetPrefix(fmt.Sprintf("[%04d-%02d-%02d %02d:%02d:%02d]",tTime.Year(), tTime.Month(), tTime.Day(),
 			tTime.Hour(), tTime.Minute(), tTime.Second()))
-		this.SetFlags(log.Llongfile)
-		Stat,_ := this.m_pFile.Stat()
+		this.m_Logger[nType].SetFlags(log.Llongfile)
+		Stat,_ := this.m_pFile[nType].Stat()
 		if Stat != nil {
 			this.m_Time = Stat.ModTime()
 		}else{
