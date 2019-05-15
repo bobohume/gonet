49322b2d18aa13fdddf1d2f3284b7a27c94f9609
bobohume <754266963@qq.com>
2019-05-15 16:22:28 +0800

    Add files via upload
    解析器过慢

---
+++ src/gonet/base/gob.go
@@ -0,0 +1,62 @@
+package base
+
+import (
+	"bytes"
+	"encoding/gob"
+)
+
+// gobencode
+type GobNode struct {
+	enc *gob.Encoder//gob解析器new耗费很大
+	dec *gob.Decoder
+	buf [2]*bytes.Buffer
+}
+
+type GobList struct {
+	m_Chan chan int
+	m_Gobs []*GobNode
+}
+
+func (this *GobList) Init(num int) {
+	this.m_Gobs = make([]*GobNode, num)
+	this.m_Chan = make(chan int, num)
+	for i := 0; i < num; i++ {
+		node := &GobNode{}
+		node.buf[0] = &bytes.Buffer{}
+		node.enc = gob.NewEncoder(node.buf[0])
+		node.buf[1] = &bytes.Buffer{}
+		node.dec = gob.NewDecoder(node.buf[1])
+		this.m_Gobs[i] = node
+		this.m_Chan <- i
+	}
+}
+
+func (this *GobList) Marsh(data interface{}) []byte{
+	n := <- this.m_Chan
+	node := this.m_Gobs[n]
+	node.buf[0].Reset()
+	node.enc.Encode(data)
+	this.m_Chan <- n
+	return node.buf[0].Bytes()
+}
+
+func (this *GobList) UnMarsh(buf []byte, data interface{}) {
+	n := <- this.m_Chan
+	node := this.m_Gobs[n]
+	node.buf[1].Reset()
+	node.buf[1].Write(buf)
+	node.dec.Decode(data)
+	this.m_Chan <- n
+}
+
+var(
+	pGob *GobList
+)
+
+func GOB() *GobList{
+	if pGob == nil{
+		pGob = new(GobList)
+		pGob.Init(10)
+	}
+	return  pGob
+}
