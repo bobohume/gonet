45a7ddeb43288f828c1abc3a2f6788fc90530e18
bobohume <754266963@qq.com>
2019-07-15 12:37:10 +0800

    11111

--- src/gonet/client/js/NewScript.js
+++
@@ -1,6 +0,0 @@
-//var Network = require("./network");
-var BitStream = require("./BitStream");
-var Packet = require("./Packet");
-var AccountMgr = require("./Account");
-var Network = require("./network");
-
