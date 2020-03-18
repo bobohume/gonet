1e0b630dffda8125cd9f14039650036ba6a7b68a
bobohume <754266963@qq.com>
2020-03-18 17:05:46 +0800

    优化ai行为树

--- src/gonet/server/common/betree/BehaviorTree.go
+++ src/gonet/server/common/betree/BehaviorTree.go
@@ -11,73 +11,82 @@ import (
 //  * Condition Node
 //  * Action Node
 type(
-	NameList struct {
+	BehaviorList struct {
 		base.Vector
 	}
 
+	IBehaviorList interface {
+		AddChild(string, IBaseNode)
+		DelChild(string)
+		GetChild(string) IBaseNode
+		GetChildCount() int
+	}
+
 	BehaviorTree struct {
-		BehaviorMap map[string] IBaseNode
-		BehaviorNameList NameList//just for sort,because range  BehaviorMap is not inorder
+		BehaviorList
 	}
 
 	IBehaviorTree interface {
+		IBehaviorList
 		Init()
-		AddNode(string, IBaseNode)
-		DelNode(string)
-		GetNode(string) IBaseNode
 		OnExec(int64)
 	}
 )
 
-func (this *NameList) Less(i, j int) bool{
-	return this.Get(i).(string) < this.Get(j).(string)
+func (this *BehaviorList) Less(i, j int) bool{
+	return this.Get(i).(IBaseNode).GetName() < this.Get(j).(IBaseNode).GetName()
 }
 
-func (this *BehaviorTree) Init(){
-	this.BehaviorMap = make(map[string] IBaseNode)
-}
-
-func (this *BehaviorTree) AddNode(name string, pNode IBaseNode){
+func (this *BehaviorList) AddChild(name string, pNode IBaseNode){
 	if pNode.GetType() != COMPOSITE && pNode.GetType() != DECORATOR && pNode.GetType() != ACTION && pNode.GetType() != CONDITION{
 		return
 	}
 
 	//pNode.Init()
-	pCurNode, exist := this.BehaviorMap[name]
-	if exist{
+	pCurNode := this.GetChild(name)
+	if pCurNode != nil{
 		if pCurNode.GetType() == COMPOSITE && pNode.(IComposite) != nil{
 			pCurNode.(IComposite).AddChild(name, pNode)
 		}else if pCurNode.GetType() == DECORATOR && pNode.(IDecorator) != nil{
 			pCurNode.(IDecorator).SetChild(pNode)
 		}
 	}else{
-		this.BehaviorMap[name] = pNode
-		this.BehaviorNameList.Push_front(name)
-		sort.Sort(&this.BehaviorNameList)
+		pNode.SetName(name)
+		this.Push_front(pNode)
+		sort.Sort(this)
 	}
 }
 
-func (this *BehaviorTree) DelNode(name string){
-	delete(this.BehaviorMap, name)
-	for i,v := range this.BehaviorNameList.Array(){
-		if v.(string) == name{
-			this.BehaviorNameList.Erase(i)
-			break
-		}
+func (this *BehaviorList) DelChild(name string){
+	nIndex := sort.Search(this.Len(), func(i int) bool {
+		return this.Get(i).(IBaseNode).GetName() >= name
+	})
+	if nIndex < this.Len() && this.Get(nIndex).(IBaseNode).GetName() == name{
+		this.Erase(nIndex)
 	}
 }
 
-func (this *BehaviorTree) GetNode(name string) IBaseNode{
-	return this.BehaviorMap[name]
+func (this *BehaviorList) GetChild(name string) IBaseNode{
+	nIndex := sort.Search(this.Len(), func(i int) bool {
+		return this.Get(i).(IBaseNode).GetName() >= name
+	})
+	if nIndex < this.Len() && this.Get(nIndex).(IBaseNode).GetName() == name{
+		return this.Get(nIndex).(IBaseNode)
+	}
+	return nil
+}
+
+func (this *BehaviorList) GetChildCount() int {
+	return this.Len()
+}
+
+func (this *BehaviorTree) Init(){
 }
 
 func (this *BehaviorTree) OnExec(tick int64){
-	for i := 0; i < this.BehaviorNameList.Len(); i++{
-		this.BehaviorMap[this.BehaviorNameList.Get(i).(string)].OnExec(tick)
+	for _,v := range this.Array() {
+		v.(IBaseNode).OnExec(tick)
 	}
-	/*for _, v := range this.BehaviorMap{
-		v.OnExec(tick)
-	}*/
 }
 
 func NewBeTree() *BehaviorTree {
--- src/gonet/server/common/betree/Composite.go
+++ src/gonet/server/common/betree/Composite.go
@@ -1,20 +1,14 @@
 package betree
 
-import "sort"
-
 type(
 	Composite struct {
 		BaseNode
-		children map[string] IBaseNode
-		childrenNameList NameList
+		BehaviorList
 	}
 
 	IComposite interface {
 		IBaseNode
-		GetChildCount() int
-		GetChild(name string) IBaseNode
-		AddChild(name string, child IBaseNode)
-		DelChild(name string)
+		IBehaviorList
 	}
 
 	Sequence struct {
@@ -26,43 +20,18 @@ type(
 	}
 
 	//并发
-	/*PSequence struct {
+	PSequence struct {
 		Composite
 	}
 
 	//并发
 	PSelector struct {
 		Composite
-	}*/
+	}
 )
 
 func (this *Composite) Init() {
-	this.children = make(map[string] IBaseNode)
 	this.Type = COMPOSITE
-}
-
-func (this *Composite) GetChildCount() int {
-	return len(this.children)
-}
-
-func (this *Composite) GetChild(name string) IBaseNode {
-	return this.children[name]
-}
-
-func (this *Composite) AddChild(name string, child IBaseNode) {
-	this.children[name] = child
-	this.childrenNameList.Push_front(name)
-	sort.Sort(&this.childrenNameList)
-}
-
-func (this *Composite) DelChild(name string) {
-	delete(this.children, name)
-	for i,v := range this.childrenNameList.Array(){
-		if v.(string) == name{
-			this.childrenNameList.Erase(i)
-			break
-		}
-	}
 }
 
 //    当执行本类型Node时，它将从begin到end迭代执行自己的Child Node：
@@ -70,18 +39,12 @@ func (this *Composite) DelChild(name string) {
 //    本Node向自己的Parent Node也返回True；否则所有Child Node都返回False，
 //    那本Node向自己的Parent Node返回False。
 func (this *Sequence) OnExec(tick int64) bool {
-	for i := 0; i < this.childrenNameList.Len(); i++{
-		if this.children[this.childrenNameList.Get(i).(string)].OnExec(tick){
+	for _,v := range this.BehaviorList.Array() {
+		if v.(IBaseNode).OnExec(tick){
 			return true
 		}
 	}
 	return false
-	/*for _,v := range this.children{
-		if v.OnExec(tick){
-			return true
-		}
-	}
-	return false*/
 }
 
 //	  当执行本类型Node时，它将从begin到end迭代执行自己的Child Node：
@@ -89,36 +52,38 @@ func (this *Sequence) OnExec(tick int64) bool {
 //    本Node向自己的Parent Node也返回False；否则所有Child Node都返回True，
 //    那本Node向自己的Parent Node返回True。
 func (this *Selector) OnExec(tick int64) bool {
-	for i := 0; i < this.childrenNameList.Len(); i++{
-		if !this.children[this.childrenNameList.Get(i).(string)].OnExec(tick){
+	for _,v := range this.BehaviorList.Array() {
+		if !v.(IBaseNode).OnExec(tick){
 			return false
 		}
 	}
 	return true
-	/*for _,v := range this.children{
-		if !v.OnExec(tick){
-			return false
-		}
-	}
-	return true*/
 }
 
-/*func (this *PSequence) OnExec(tick int64) bool {
+//    当执行本类型Node时，它将从begin到end迭代执行自己的Child Node：
+//    如遇到一个Child Node执行后返回True，那停止迭代，
+//    本Node向自己的Parent Node也返回True；否则所有Child Node都返回False，
+//    那本Node向自己的Parent Node返回False。
+func (this *PSequence) OnExec(tick int64) bool {
 	bScuess := true
-	for _,v := range this.children{
-		if !v.OnExec(tick){
+	for _,v := range this.BehaviorList.Array() {
+		if !v.(IBaseNode).OnExec(tick){
 			bScuess = false
 		}
 	}
 	return bScuess
 }
 
+//	  当执行本类型Node时，它将从begin到end迭代执行自己的Child Node：
+//    如遇到一个Child Node执行后返回False，那停止迭代，
+//    本Node向自己的Parent Node也返回False；否则所有Child Node都返回True，
+//    那本Node向自己的Parent Node返回True。
 func (this *PSelector) OnExec(tick int64) bool {
 	bScuess := false
-	for _,v := range this.children{
-		if v.OnExec(tick){
+	for _,v := range this.BehaviorList.Array() {
+		if v.(IBaseNode).OnExec(tick){
 			bScuess = true
 		}
 	}
 	return bScuess
-}*/
+}
