e23b0521ca0d81120404bc3c06519e954429f6b1
bobohume <754266963@qq.com>
2021-02-28 20:21:28 +0800

    11

--- common/betree/Composite.go
+++ common/betree/Composite.go
@@ -34,24 +34,11 @@ func (this *Composite) Init() {
 	this.Type = COMPOSITE
 }
 
-//    当执行本类型Node时，它将从begin到end迭代执行自己的Child Node：
-//    如遇到一个Child Node执行后返回True，那停止迭代，
-//    本Node向自己的Parent Node也返回True；否则所有Child Node都返回False，
-//    那本Node向自己的Parent Node返回False。
-func (this *Sequence) OnExec(tick int64) bool {
-	for _,v := range this.BehaviorList.Values() {
-		if v.(IBaseNode).OnExec(tick){
-			return true
-		}
-	}
-	return false
-}
-
 //	  当执行本类型Node时，它将从begin到end迭代执行自己的Child Node：
 //    如遇到一个Child Node执行后返回False，那停止迭代，
 //    本Node向自己的Parent Node也返回False；否则所有Child Node都返回True，
 //    那本Node向自己的Parent Node返回True。
-func (this *Selector) OnExec(tick int64) bool {
+func (this *Sequence) OnExec(tick int64) bool {
 	for _,v := range this.BehaviorList.Values() {
 		if !v.(IBaseNode).OnExec(tick){
 			return false
@@ -64,25 +51,38 @@ func (this *Selector) OnExec(tick int64) bool {
 //    如遇到一个Child Node执行后返回True，那停止迭代，
 //    本Node向自己的Parent Node也返回True；否则所有Child Node都返回False，
 //    那本Node向自己的Parent Node返回False。
-func (this *PSequence) OnExec(tick int64) bool {
-	bScuess := true
+func (this *Selector) OnExec(tick int64) bool {
 	for _,v := range this.BehaviorList.Values() {
-		if !v.(IBaseNode).OnExec(tick){
-			bScuess = false
+		if v.(IBaseNode).OnExec(tick){
+			return true
 		}
 	}
-	return bScuess
+	return false
 }
 
 //	  当执行本类型Node时，它将从begin到end迭代执行自己的Child Node：
 //    如遇到一个Child Node执行后返回False，那停止迭代，
 //    本Node向自己的Parent Node也返回False；否则所有Child Node都返回True，
 //    那本Node向自己的Parent Node返回True。
-func (this *PSelector) OnExec(tick int64) bool {
+func (this *PSequence) OnExec(tick int64) bool {
 	bScuess := false
 	for _,v := range this.BehaviorList.Values() {
 		if v.(IBaseNode).OnExec(tick){
 			bScuess = true
+		}
+	}
+	return bScuess
+}
+
+//    当执行本类型Node时，它将从begin到end迭代执行自己的Child Node：
+//    如遇到一个Child Node执行后返回True，那停止迭代，
+//    本Node向自己的Parent Node也返回True；否则所有Child Node都返回False，
+//    那本Node向自己的Parent Node返回False。
+func (this *PSelector) OnExec(tick int64) bool {
+	bScuess := true
+	for _,v := range this.BehaviorList.Values() {
+		if !v.(IBaseNode).OnExec(tick){
+			bScuess = false
 		}
 	}
 	return bScuess
